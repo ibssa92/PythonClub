9c5435ac961527a0d0255a9313da80b66bda1f0d
ibssa92 <41655642+ibssa92@users.noreply.github.com>
2019-05-13 16:05:56 -0700

    a6 Meeting and Detail

--- PythonClub/Club/urls.py
+++ PythonClub/Club/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
-    path('getMeetings/', views.getMeetings, name='meetings')
+    path('getMeetings/', views.getMeetings, name='meetings'),
+    path('getEvents/', views.getEvents, name='events'),
+    path('meetingDetail/<int:id>',views.meetingDetail, name='meetingdetail'),
 ]
--- PythonClub/Club/views.py
+++ PythonClub/Club/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Event, Meeting, MeetingMinutes, Resource
 
 # Create your views here.
@@ -6,6 +6,23 @@ def index(request):
     return render(request, 'Club/index.html')
 
 def getMeetings(request):
-    meetings_list=Event.objects.all()
+    meetings_list=Meeting.objects.all()
     context={'meetings_list' : meetings_list}
     return render(request, 'Club/meetings.html', context=context)
+
+def getEvents(request) :
+    events_list=Event.objects.all()
+    return render(request, 'Club/events.html', {'events_list' : events_list})
+
+def meetingDetail (request, id): 
+    meet=get_object_or_404(Meeting, pk=id)
+    minutescount=MeetingMinutes.objects.filter(meetingID=id).count()
+    minutes=MeetingMinutes.objects.filter(meetingID=id)
+    context={
+        'meet' : meet, 
+        'minutescount' : minutescount,
+        'minutes' : minutes,
+
+    }
+
+    return render (request, 'Club/meetingdetail.html', context=context)
