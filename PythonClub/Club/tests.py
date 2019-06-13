47f78ffb8e3c28c7b4067dc2013fa45247efddb8
ibssa92 <41655642+ibssa92@users.noreply.github.com>
2019-06-13 14:49:43 -0700

    a10 More Tests

--- PythonClub/Club/tests.py
+++ PythonClub/Club/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from .models import Event, Meeting, MeetingMinutes, Resource
+import datetime
+from .forms import MeetingForm
 
 # Create your tests here.
 class MeetingTest(TestCase) :
@@ -26,3 +28,29 @@ class MeetingMinutesTest (TestCase):
     def test_table(self):
         self.assertEqual(str(MeetingMinutes._meta.db_table),'meeting')
 
+class MeetingFormTest(TestCase):
+    def setUp(self):
+        self.user=User.objects.create('username=user1', password='P@ssw0rd1')
+        self.type=MeetingType.objects.create(meetingtypename='type1')
+    
+    def test_meetingForm(self):
+        data={
+            'meetingtitle' : 'meeting1',
+            'meetingagenda' : 'agenda1',
+            'meetinglocation' : 'location1', 
+           
+
+        }
+        form = MeetingForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_meetingFormInvalid(self):
+        data={
+            'meetingtitle' : 'meeting1',
+            'meetingagenda' : '',
+            'meetinglocation' : 'location1', 
+           
+
+        }
+        form = MeetingForm(data=data)
+        self.assertFalse(form.is_valid())
